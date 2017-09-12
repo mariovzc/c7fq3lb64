0f6a36a92c99e72e904d28ed0ee29e23eb5e14d3
¨Mario <mario.vizcainos@gmail.com>
2017-09-12 16:00:00 -0500

    filter by user

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,10 @@
 class Expense < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
-  def self.search(concept: nil, category_id: nil)
+  def self.search(concept: nil, category_id: nil, user: current_user)
     with_concept(concept)
       .with_category_id(category_id)
+      .with_user(user)
   end
   scope :with_concept, proc { |concept|
     if concept.present?
@@ -17,7 +18,7 @@ class Expense < ActiveRecord::Base
   }
   scope :with_user, proc { |user|
     if user.present?
-      where(user: user)
+      where(user_id: user.id)
     end
   }
 
